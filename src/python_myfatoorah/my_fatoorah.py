24b15268d190c937c90f0db82d4c222d24d26801
Kamruzzaman Tauhid <tauhid@Kamruzzamans-MacBook-Pro.local>
2024-09-05 18:14:20 +0600

    Initiate payment function

--- src/python_myfatoorah/my_fatoorah.py
+++ src/python_myfatoorah/my_fatoorah.py
@@ -1,5 +1,8 @@
 import requests
+
 from requests.models import Response
+
+from typing import Optional
 
 class MyFatoorah:
     def __init__(self, base_url: str, api_token: str):
@@ -14,6 +17,15 @@ class MyFatoorah:
         return requests.post(api_url, json=payload, headers=self.headers)
     
 
+    def initiate_payment(self, invoice_amount: float, currency_iso: str) -> Response:
+        payload: dict = {
+            'InvoiceAmount': invoice_amount,
+            'CurrencyIso': currency_iso
+        }
+
+        return self.send_request(self.base_url + 'InitiatePayment', payload)
+
+
     def initiate_session(self, customer_id: str, save_token: str=False) -> Response:
         payload: dict = {
             'CustomerIdentifier': customer_id
@@ -25,7 +37,7 @@ class MyFatoorah:
         return self.send_request(self.base_url + 'InitiateSession', payload)
     
 
-    def execute_payment(self, invoice_value: float, session_id: str=None, payment_method_id: int=None, display_currency_iso: str=None, callback_url: str=None) -> Response:
+    def execute_payment(self, invoice_value: float, session_id: Optional[str]=None, payment_method_id: Optional[int]=None, display_currency_iso: str=None, callback_url: str=None) -> Response:
         if all([session_id is None, payment_method_id is None]):
             raise Exception('Either payment_method_id or session_id is required')
 
